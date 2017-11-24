a2e779acfdf89a3f14048cef806d7df201c73a5a
Steven Pelletier <stevenpelletier@knights.ucf.edu>
2017-11-24 01:10:09 -0500

    about

--- about.php
+++ about.php
@@ -6,51 +6,6 @@ function customPageHeader(){?>
 <?php } include("includes/header.php"); ?>
 <?php include("includes/navbar.php"); ?>
 
-
-    <header>
-        <div id="carouselHome" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselHome" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselHome" data-slide-to="1"></li>
-                <li data-target="#carouselHome" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner" role="listbox">
-                <!-- Slide One - Set the background image for this slide in the line below -->
-                <div class="carousel-item active" style="background-image: url('images/header1.jpg')">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h1>SeaAngels</h1>
-                        <h3 class="slider-captions">First Slide</h3>
-                        <p>This is a description for the first slide.</p>
-                    </div>
-                </div>
-                <!-- Slide Two - Set the background image for this slide in the line below -->
-                <div class="carousel-item" style="background-image: url('images/header2.jpg')">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h2>CHYEAH</h2>
-                        <h3 class="slider-captions">Second Slide</h3>
-                        <p>This is a description for the second slide.</p>
-                    </div>
-                </div>
-                <!-- Slide Three - Set the background image for this slide in the line below -->
-                <div class="carousel-item" style="background-image: url('images/pelican.jpg')">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h1>Hey Im Mr Poopy Butthole</h1>
-                        <h3 class="slider-captions">Third Slide</h3>
-                        <p>This is a description for the third slide.</p>
-                    </div>
-                </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselHome" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselHome" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
-        </div>
-    </header>
-
 <!-- Page Content -->
     <div class="container">
 
