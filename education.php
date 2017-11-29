0ef0409391f3d681efaa15630e3ce083a3e17cb9
Steven Pelletier <stevenpelletier@knights.ucf.edu>
2017-11-29 01:37:40 -0500

    education page

--- education.php
+++ education.php
@@ -7,122 +7,176 @@ function customPageHeader(){?>
     <?php include("includes/navbar.php"); ?>
     <div class="container">
         <!-- Page Heading -->
-        <h1 class="my-4">Education <small>Info</small>
-        </h1>
-        <div class="row">
-            <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
-                <div class="card h-100">
-                    <a href="#"><img class="card-img-top" src="images/gallery/balloon.png" alt=""></a>
-                    <div class="card-body">
-                        <h4 class="card-title">
-                            <a href="#">Balloons</a>
-                        </h4>
-                        <p class="card-text">The balloon industry claims balloons degrade as fast as an oak leaf. If this were true, balloons would take up to four years to fully degrade - much too long a time for them to be around to be found and eaten by an unsuspecting animal.</p>
-                    </div>
+
+                <h1 class="text-center" id="education">Did You Know?</h1>
+                <h3 class="text-center" id="education2">18,000 pieces of plastic litter are floating on every square mile of the world's oceans. <br> 8 million tons of debris enters the world's oceans every year.  </h3>
+                <p class="text-center"> To quote Sea Shepherd's Captain Paul Watson, "If the Oceans Die, We Die". Awareness begins with education. Take a look at some of the most common sources of litter and how they affect the environment.</p>
+               
+
+    <div class="row" id="litterinfo">
+        <div class ="col-md-6 text-left ">
+
+        
+            <div class="row">
+                <div class ="col-md-2 text-left ">
+                    <span><img src="images/balloons.png"></span>
+
+                </div>
+                <div class ="col-md-10 text-left ">
+                    <h3>Balloons</h3>
+                    <span class="para_text" style="">The balloon industry claims balloons degrade as fast as an oak leaf. If this were true, balloons would take up to four years to fully degrade - much too long a time for them to be around to be found and eaten by an unsuspecting animal.</span>
+                </div>
+            
+            </div>
+
+            <div class="row">
+                <div class ="col-md-2 text-left ">
+                    <span><img src="images/cigarette.png"></span>
+
+                </div>
+                <div class ="col-md-10 text-left ">
+                    <h3 class="top_title" style="">Cigarettes</h3>
+                    <span class="para_text" style="">The most littered item worldwide are cigarette butts. They get thrown out of car windows, discarded in the sand on the beach and are seen every where from parking lots to playgrounds.</span>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
-                <div class="card h-100">
-                    <a href="#"><img class="card-img-top" src="images/gallery/cigarettes.png" alt=""></a>
-                    <div class="card-body">
-                        <h4 class="card-title">
-                            <a href="#">Cigarettes</a>
-                        </h4>
-                        <p class="card-text">The most littered item worldwide are cigarette butts. They get thrown out of car windows, discarded in the sand on the beach and are seen every where from parking lots to playgrounds.</p>
-                    </div>
+
+            <div class="row">  
+                <div class ="col-md-2 text-left ">
+                      <span><img src="images/wine-bottle.png"></span>
+
+                </div>
+                <div class ="col-md-10 text-left ">
+                    <h3 class="top_title" style="">Glass</h3>
+                    <span class="para_text" style="">Recycling one glass bottle saves enough energy to light a 100 watt light bulb for four hours or power a computer for 30 minutes. That’s just one glass bottle.</span>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
-                <div class="card h-100">
-                    <a href="#"><img class="card-img-top" src="images/gallery/glass.png" alt=""></a>
-                    <div class="card-body">
-                        <h4 class="card-title">
-                            <a href="#">Glass</a>
-                        </h4>
-                        <p class="card-text"> Recycling one glass bottle saves enough energy to light a 100 watt light bulb for four hours or power a computer for 30 minutes. That’s just one glass bottle.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
-                <div class="card h-100">
-                    <a href="#"><img class="card-img-top" src="images/gallery/plastic.png" alt=""></a>
-                    <div class="card-body">
-                        <h4 class="card-title">
-                            <a href="#">Plastic</a>
-                        </h4>
-                        <p class="card-text">Every form of plastic can be a danger to our environment and plastic bags are no exception. This bird was a victim of a lttered plastic bag.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
-                <div class="card h-100">
-                    <a href="#"><img class="card-img-top" src="images/gallery/styrofoam.png" alt=""></a>
-                    <div class="card-body">
-                        <h4 class="card-title">
-                            <a href="#">Styrofoam</a>
-                        </h4>
-                        <p class="card-text">Cups, packing material, plates and other items have been made from polystyrene or styrofoam as it is known as well. This material can be an ecological nightmare if not disposed of properly. </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
-                <div class="card h-100">
-                    <a href="#"><img class="card-img-top" src="images/gallery/medical_waste.png" alt=""></a>
-                    <div class="card-body">
-                        <h4 class="card-title">
-                            <a href="#">Medical Waste</a>
-                        </h4>
-                        <p class="card-text">Thoughts of medical waste beckon images of red bio-hazard bags of potentially dangerous materials. Medical waste is solid waste created by diagnosing, treating or immunizing people or animals</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
-                <div class="card h-100">
-                    <a href="#"><img class="card-img-top" src="images/gallery/fouRs.png" alt=""></a>
-                    <div class="card-body">
-                        <h4 class="card-title">
-                            <a href="#">The "4 R's"</a>
-                        </h4>
-                        <p class="card-text">Reduce, Reuse, Recycle and Refuse.Every year, Americans throw away 50 billion food and drink cans, 27 billion glass bottles and jars, and 65 million plastic and metal jar and can covers. More than 30% of our waste is packaging materials. </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
-                <div class="card h-100">
-                    <a href="#"><img class="card-img-top" src="images/gallery/cleanup_tips.png" alt=""></a>
-                    <div class="card-body">
-                        <h4 class="card-title">
-                            <a href="#">Cleanup Tips</a>
-                        </h4>
-                        <p class="card-text">It may seem like a simple concept to clean up the beach. Generally it is, but if you want to do it safely and in a green way, here are some helpful tips.</p>
-                    </div>
-                </div>
-            </div>
+    
         </div>
-        <!-- /.row -->
-        <!-- Pagination -->
-        <ul class="pagination justify-content-center">
-            <li class="page-item">
-                <a class="page-link" href="#" aria-label="Previous">
-                    <span aria-hidden="true">&laquo;</span>
-                    <span class="sr-only">Previous</span>
-                </a>
-            </li>
-            <li class="page-item">
-                <a class="page-link" href="#">1</a>
-            </li>
-            <li class="page-item">
-                <a class="page-link" href="#">2</a>
-            </li>
-            <li class="page-item">
-                <a class="page-link" href="#">3</a>
-            </li>
-            <li class="page-item">
-                <a class="page-link" href="#" aria-label="Next">
-                    <span aria-hidden="true">&raquo;</span>
-                    <span class="sr-only">Next</span>
-                </a>
-            </li>
-        </ul>
+            
+        <div class ="col-md-6 text-right img-padding-top ">
+        <figure class="figure">
+  <img src="images/gallery/plastic.png" class="figure-img img-fluid rounded"  id="birdlitter" alt="Bird Caught In Littered Plastic">
+  <figcaption class="figure-caption">Every form of plastic can be a danger to our environment and plastic bags are no exception. This bird was a victim of a lttered plastic bag.</figcaption>
+</figure>
+        
+        </div>
+
+        
+
+    </div> 
+
+
+                <h1 class="text-center">Getting Familiar With Native Wildlife</h1>
+                
+                <p class="text-center">Learn about the effects our trash has on our marine and wild life. Learn what you can do to help them. This page will give information about the animals and the risks that have befallen them.</p>
+
+
+                   <div class="row" id="wildifeinfo">
+            <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
+                <div class="card h-80">
+                    <a href="#"><img class="card-img-top" src="images/gallery/alligator.png" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            Alligators
+                        </h4>
+                        <p class="card-text">Alligators were hunted at one time almost to the point of extinction.  Because of this alligators are protected by both state and federal regulations. It is illegal to hunt or trap a wild alligator without a permit.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
+                <div class="card h-80">
+                    <a href="#"><img class="card-img-top" src="images/gallery/dolphin.png" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                           Dolphins
+                        </h4>
+                        <p class="card-text"> Our seas are blessed with many magnificent creatures.  Among them are the beautiful and graceful dolphin.  Known for their intelligence and their docile nature they are truly a treasure.   </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
+                <div class="card h-80">
+                    <a href="#"><img class="card-img-top" src="images/gallery/Manatee.png" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            Manatees
+                        </h4>
+                        <p class="card-text">Manatees are such wonderful creatures.  A gentle giant of sorts they are endangered, by federal guidelines, partly do to human contact or encroachment, boating accidents, litter and more.  </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
+                <div class="card h-80">
+                    <a href="#"><img class="card-img-top" src="images/gallery/turtle.png" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            Turtles
+                        </h4>
+                        <p class="card-text">Florida is the second largest sea turtle nesting area in the world.  Oman in southwest Asia is the first.  Sea Turtle season starts March 1st and ends October 31</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
+                <div class="card h-80">
+                    <a href="#"><img class="card-img-top" src="images/gallery/cormorant.png" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            Cormorants
+                        </h4>
+                        <p class="card-text">Cormorants are coastal rather than oceanic birds, and some have colonised inland waters – indeed, the original ancestor of cormorants seems to have been a fresh-water bird, judging from the habitat of the most ancient lineage. </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
+                <div class="card h-80">
+                    <a href="#"><img class="card-img-top" src="images/gallery/snowy_egret.png" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            Snowy Egret
+                        </h4>
+                    
+                        <p class="card-text">Snowy egrets nest in colonies on thick vegetation in isolated places, such as barrier islands, dredge-spoil islands, salt marsh islands, swamps, and marshes. They often change location from year to year. </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
+                <div class="card h-80">
+                    <a href="#"><img class="card-img-top" src="images/gallery/spoonbill.png" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            Spoonbill
+                        </h4>
+                    
+                        <p class="card-text">All spoonbills have large, flat, spatulate bills and feed by wading through shallow water, sweeping the partly opened bill from side to side.</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-lg-3 col-md-4 col-sm-6 portfolio-item">
+                <div class="card h-80">
+                    <a href="#"><img class="card-img-top" src="images/gallery/tri_color_heron.png" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            Tri Colored Heron
+                        </h4>
+                    
+                        <p class="card-text">Tricolored heron's breeding habitat is sub-tropical swamps. It nests in colonies, often with other herons, usually on platforms of sticks in trees or shrubs. In each clutch, 3–7 eggs are typically laid. </p>
+                    </div>
+                </div>
+            </div>
+        
+        </div>
+
+               
+
+</div>
+
+ 
+
+
+        </div> <!-- container -->
+ 
     </div>
     <?php include("includes/footer.php"); ?>
+
+    
