78c1711f8d99a6867fb0652096d10f78ea51fd8e
Steven Pelletier <stevenpelletier@knights.ucf.edu>
2017-11-30 18:47:49 -0500

    navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,32 +1,39 @@
-    <nav class="navbar navbar-expand-md navbar-light" id="nav" style="background-color: #00bcd4">
-        
-        <a class="navbar-brand" href="index.php"><img alt="SeaAngels Green Beach Clean Logo" class="d-inline-block align-top animated fadeInLeft" height="75" src="images/logohead2.png"></a>
-        <button aria-controls="expanded_nav" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-target="#expanded_nav" data-toggle="collapse" type="button">
+<nav class="navbar navbar-expand-md navbar-light" id="nav" style="background-color: #00bcd4">
+    <a class="navbar-brand" href="index.php"><img alt="SeaAngels Green Beach Clean Logo" class="d-inline-block align-top animated fadeInLeft" height="75" src="images/logohead2.png"></a>
+    <button aria-controls="expanded_nav" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-target="#expanded_nav" data-toggle="collapse" type="button">
         <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="expanded_nav">
-            <ul class="navbar-nav mr-auto animated fadeIn" id="nav_anim">
-                <li class="nav-item">
-                    <a class="nav-link <?php if($page=='home'){echo 'active';}?>" href="index.php">HOME</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link <?php if($page=='about'){echo 'active';}?>" href="about.php">ABOUT</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link <?php if($page=='schedule'){echo 'active';}?>" href="schedule.php">SCHEDULE</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link <?php if($page=='education'){echo 'active';}?>" href="education.php">EDUCATION</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link <?php if($page=='contact'){echo 'active';}?>" href="contact.php">CONTACT US</a>
-                </li>
-            </ul>
-            <div class="nav-items-right animated fadeInRight">
-                <a href="https://www.facebook.com/SeaAngelsBeachClean/" target="_blank"><i aria-hidden="true" class="fa fa-facebook-official fa-2x sm-icon"></i></a>
-                <a href="https://twitter.com/" target="_blank"><i aria-hidden="true" class="fa fa-twitter fa-2x sm-icon"></i></a>
-                <a href="https://www.instagram.com/" target="_blank"><i aria-hidden="true" class="fa fa-instagram fa-2x sm-icon"></i></a>
-                <a href="#" class="btn btn-info mx-5 mb-2" role="button">Donate</a>
-            </div>
+    </button>
+    <div class="collapse navbar-collapse" id="expanded_nav">
+        <ul class="navbar-nav mr-auto animated fadeIn" id="nav_anim">
+            <li class="nav-item">
+                <a class="nav-link <?php if($page=='home'){echo 'active';}?>" href="index.php">HOME</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle <?php if($page=='about'){echo 'active';}?>" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">ABOUT
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="about.php">About SeaAngels</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="media.php">News and Media</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="gallery.php">Gallery</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link <?php if($page=='schedule'){echo 'active';}?>" href="schedule.php">SCHEDULE</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link <?php if($page=='education'){echo 'active';}?>" href="education.php">EDUCATION</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link <?php if($page=='contact'){echo 'active';}?>" href="contact.php">CONTACT US</a>
+            </li>
+        </ul>
+        <div class="nav-items-right animated fadeInRight">
+            <a href="https://www.facebook.com/SeaAngelsBeachClean/" target="_blank"><i aria-hidden="true" class="fa fa-facebook-official fa-2x sm-icon"></i></a>
+            <a href="https://twitter.com/" target="_blank"><i aria-hidden="true" class="fa fa-twitter fa-2x sm-icon"></i></a>
+            <a href="https://www.instagram.com/" target="_blank"><i aria-hidden="true" class="fa fa-instagram fa-2x sm-icon"></i></a>
+            <a href="#" class="btn btn-info mx-5 mb-2" role="button">Donate</a>
         </div>
-    </nav>
+    </div>
+</nav>
---
+++ media.php
@@ -0,0 +1,85 @@
+<?php
+$page='about';
+$PageTitle="Media and Videos";
+
+function customPageHeader(){?>
+<?php } include("includes/header.php"); ?>
+<?php include("includes/navbar.php"); ?>
+
+<!-- Page Content -->
+    <div class="container">
+
+      <!-- Introduction Row -->
+      <h1 class="my-4">About Us
+        <small>It's Nice to Meet You!</small>
+      </h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint, explicabo dolores ipsam aliquam inventore corrupti eveniet quisquam quod totam laudantium repudiandae obcaecati ea consectetur debitis velit facere nisi expedita vel?</p>
+
+       <div class="row">
+        <div class="col-md-4">
+          <a href="#">
+            <img class="img-fluid rounded mb-3 mb-md-0" src="images/test.png" alt="">
+          </a>
+        </div>
+        <div class="col-md-8">
+          <h3>Project One</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
+          <a class="btn btn-primary" href="#">View Project</a>
+        </div>
+      </div>
+      
+      <hr>
+
+      <!-- Team Members Row -->
+      <div class="row">
+        <div class="col-lg-12">
+          <h2 class="my-4">Our Team</h2>
+        </div>
+        <div class="col-lg-4 col-sm-6 text-center mb-4">
+          <img class="rounded-circle img-fluid d-block mx-auto" src="http://www.evanmarckatz.com/blog/wp-content/uploads/2009/06/Blog-pic-40-yr-old-man-2197365.jpg" alt="">
+          <h3>John Smith
+            <small>Job Title</small>
+          </h3>
+          <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+        </div>
+        <div class="col-lg-4 col-sm-6 text-center mb-4">
+          <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
+          <h3>John Smith
+            <small>Job Title</small>
+          </h3>
+          <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+        </div>
+        <div class="col-lg-4 col-sm-6 text-center mb-4">
+          <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
+          <h3>John Smith
+            <small>Job Title</small>
+          </h3>
+          <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+        </div>
+        <div class="col-lg-4 col-sm-6 text-center mb-4">
+          <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
+          <h3>John Smith
+            <small>Job Title</small>
+          </h3>
+          <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+        </div>
+        <div class="col-lg-4 col-sm-6 text-center mb-4">
+          <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
+          <h3>John Smith
+            <small>Job Title</small>
+          </h3>
+          <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+        </div>
+        <div class="col-lg-4 col-sm-6 text-center mb-4">
+          <img class="rounded-circle img-fluid d-block mx-auto" src="http://placehold.it/200x200" alt="">
+          <h3>John Smith
+            <small>Job Title</small>
+          </h3>
+          <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+        </div>
+      </div>
+
+    </div>
+    <!-- /.container -->
+
+<?php include("includes/footer.php"); ?>
