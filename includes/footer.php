562ae14d5c7c4fed3acc036d3ca53c5f317e54f8
Steven Pelletier <stevenpelletier@knights.ucf.edu>
2017-12-01 19:28:32 -0500

    validation

--- includes/footer.php
+++ includes/footer.php
@@ -1,74 +1,70 @@
-
-
-  <footer class="py-5" style="background-image:url('images/wave_footer_4.svg'); background-position:center; background-size: cover; overflow-x: hidden ">
-    <div class="container">
-        <div class="row px-">
-             <div class="col-sm-4 text-center"style="margin-top:120px;">
-                    <div class="footer-items">
-                        <h4 style="color:white;">NAVIGATION</h4>
-                        <ul class="list " style="list-style-type:none;">
-                            <li><a href="index.php" style="color: #fff;">Home</a></li>
-                            <li><a href="calendar.php" style="color: #fff;">Schedule</a></li>
-                            <li><a href="about.php" style="color: #fff;">About</a></li>
-                            <li><a href="education.php" style="color: #fff;">Education</a></li>
-                            <li><a href="contact.php" style="color: #fff;">Contact Us</a></li>
-                        </ul>
-                    </div>
-                </div>
-
-
-                <div class="col-sm-4 text-center" style="margin-top:120px;">
-                    <div class="footer-items">
-                    <h4 style="color:white;">SUPPORT SEA ANGELS</h4></div>
-                    
-                    <p class="" style="color:white;"> Our organization relies on the generosity of individuals like you in helping us to fight against the plight of litter in our environment. Want to help us grow? Please consider a donation to our cause.</p>
-                    <a href="https://www.paypal.com/donate/?token=30aE1T-io_JvWnxs2RV5Q_lHC0FKoh4ZPbD2LkPbI3CecUrjlnSWAYdh9NV7b6O0GgiTGG&country.x=US&locale.x=" class="btn btn-success mx-5 mb-2" role="button">Click Here To Donate</a>
-                </div>
-
-
-                <div class="col-sm-4 text-center" style="margin-top:120px;">
-                    <div class="footer-items">
-                        <h4 style="color:white;">STAY CONNECTED</h4>
-                        <p>Sign Up to receive information about meetups, updates, events and more!</p>
-                            <form style="padding-left:60px;">
-                                <div class="form-group row">
-                                    <label for="nameiput" class="col-2  form-control-sm col-form-label">Name</label><br>
-                                    <input type="text" class="form-control-sm col-form-label" id="nameinput" aria-describedby="emailHelp" placeholder="Enter Name">
-                                </div>
-                
-                             <div class="form-group row">
-                                <label for="emailinput" class="col-2  form-control-sm col-form-label">Email</label>
-                                <input type="email" class="form-control-sm col-form-label" id="emailinput" placeholder="E-mail">
-                            </div>
-  
-                        <button type="submit" class="btn btn-sm btn-primary">Sign Me Up!</button>
-                         </form>
-                    </div>
-                   
-                    </div>
-                    </div>
-        
-    
-
-
-         <div class="row">
-                <div class="col-12">
-                        <p class="text-center" style=" font-size:13px;"> Copyright &copy; SeaAngels Green Beach Clean 2017</p>
-                </div>
+<footer class="py-5" style="background-image:url('images/wave_footer_4.svg'); background-position:center; background-size: cover; overflow-x: hidden">
+  <div class="container">
+    <div class="row px-">
+      <div class="col-sm-4 text-center" style="margin-top:120px;">
+        <div class="footer-items">
+          <h4 style="color:white;">NAVIGATION</h4>
+          <ul class="list" style="list-style-type:none;">
+            <li>
+              <a href="index.php" style="color: #fff;">Home</a>
+            </li>
+            <li>
+              <a href="calendar.php" style="color: #fff;">Schedule</a>
+            </li>
+            <li>
+              <a href="about.php" style="color: #fff;">About</a>
+            </li>
+            <li>
+              <a href="education.php" style="color: #fff;">Education</a>
+            </li>
+            <li>
+              <a href="contact.php" style="color: #fff;">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-sm-4 text-center" style="margin-top:120px;">
+        <div class="footer-items">
+          <h4 style="color:white;">SUPPORT SEA ANGELS</h4>
+        </div>
+        <p class="" style="color:white;">Our organization relies on the generosity of individuals like you in helping us to fight against the plight of litter in our environment. Want to help us grow? Please consider a donation to our cause.</p><a class="btn btn-success mx-5 mb-2" href="https://www.paypal.com/donate/?token=30aE1T-io_JvWnxs2RV5Q_lHC0FKoh4ZPbD2LkPbI3CecUrjlnSWAYdh9NV7b6O0GgiTGG&country.x=US&locale.x=" role="button">Click Here To Donate</a>
+      </div>
+      <div class="col-sm-4 text-center" style="margin-top:120px;">
+        <div class="footer-items">
+          <h4 style="color:white;">STAY CONNECTED</h4>
+          <p>Sign Up to receive information about meetups, updates, events and more!</p>
+          <form style="padding-left:60px;">
+            <div class="form-group row">
+              <label class="col-2 form-control-sm col-form-label" for="nameiput">Name</label><br>
+              <input aria-describedby="emailHelp" class="form-control-sm col-form-label" id="nameinput" placeholder="Enter Name" type="text">
             </div>
-    </footer>
-    
-
-
-
-
-    <script src="scripts/jquery.min.js"></script>
-    <script src="scripts/popper.min.js"></script>
-    <script src="scripts/bootstrap.min.js"></script>
-    <script src="scripts/moment.min.js"></script>
-    <script src="scripts/fullcalendar.min.js"></script>
-    <script src="scripts/gcal.min.js"></script>
-    <script src="scripts/main.js"></script>
+            <div class="form-group row">
+              <label class="col-2 form-control-sm col-form-label" for="emailinput">Email</label> <input class="form-control-sm col-form-label" id="emailinput" placeholder="E-mail" type="email">
+            </div><button class="btn btn-sm btn-primary" type="submit">Sign Me Up!</button>
+          </form>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <p class="text-center" style=" font-size:13px;">Copyright &copy; SeaAngels Green Beach Clean 2017</p>
+      </div>
+    </div>
+  </div>
+</footer>
+<script src="scripts/jquery.min.js">
+</script> 
+<script src="scripts/popper.min.js">
+</script> 
+<script src="scripts/bootstrap.min.js">
+</script> 
+<script src="scripts/moment.min.js">
+</script> 
+<script src="scripts/fullcalendar.min.js">
+</script> 
+<script src="scripts/gcal.min.js">
+</script> 
+<script src="scripts/main.js">
+</script>
 </body>
-
 </html>
